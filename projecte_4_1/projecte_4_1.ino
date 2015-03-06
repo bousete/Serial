b7d5c56cac53a71d80fd7ed9587a7ecad228118d
bousete <bouso_13@gmail.com>
2015-03-06 20:12:10 +0100

    Proves github
    Borrar i editar coses

--- projecte_4_1/projecte_4_1.ino
+++ projecte_4_1/projecte_4_1.ino
@@ -1,16 +1,16 @@
-int tempAigua = 91;
+int tempAiguas = 91;
 
 void setup()              // run once, when the sketch starts
 {
   Serial.begin(9600);     // set up Serial library at 9600 bps
 
-  if ( tempAigua > 100)
+  if ( tempAiguas > 100)
   {
     Serial.print("Aigua supera els 100C, esta bullint!");
   } 
 }
 
-void loop()   // we need this to be here even though its empty
+void loop()   // need this to be here even though its empty
 {
 }
 
